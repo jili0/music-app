17b3e53bbf9d169afdd2cabf257d3b5af79fa0bf
Jing <jingli0319@gmail.com>
2024-10-07 12:27:22 +0200

    style the searchbar

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("hello test");
+//Musikdaten als Array hinzufügen
+//jl: toggle searchbar
+const showSearchbar = () => {
+    if (searchbar)
+        searchbar.style.animationName === "hideSearchbar" ? searchbar.style.animationName = "showSearchbar" : searchbar.style.animationName = "hideSearchbar";
+};
+const searchBtn = document.getElementById("searchBtn");
+const searchbar = document.getElementById("searchbar");
+searchBtn === null || searchBtn === void 0 ? void 0 : searchBtn.addEventListener("click", showSearchbar);
--- main.ts
+++ main.ts
@@ -3,3 +3,12 @@
 interface Artist {
     external_urls: { spotify: string}
 }
+
+//jl: toggle searchbar
+const showSearchbar = () => {
+  if (searchbar) searchbar.style.animationName === "hideSearchbar" ? searchbar.style.animationName = "showSearchbar" : searchbar.style.animationName = "hideSearchbar" ;
+
+}
+const searchBtn = document.getElementById("searchBtn")
+const searchbar = document.getElementById("searchbar")
+searchBtn?.addEventListener("click", showSearchbar)
