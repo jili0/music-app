96dedce13e6a074be08c1498c728e840a2b7e9ca
Jing <jingli0319@gmail.com>
2024-10-20 16:16:30 +0200

    refactor

--- main.js
+++ main.js
@@ -8,13 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let playlist;
-let localStoragePlaylist;
-let playlistCurrentSong;
-let playlistCurrentSongLikeBtn;
-let isPlaying = false;
-let isShuffling = false;
-let isRepeating = false;
 // body container
 const container = document.getElementById("container");
 // header btn and container
@@ -43,6 +36,14 @@ const likeBtn = document.getElementById("like");
 const shuffleBtn = document.getElementById("shuffle");
 const repeatOneBtn = document.getElementById("repeatOne");
 const settingsBtn = document.getElementById("settings");
+let playlist;
+let localStoragePlaylist = JSON.parse(localStorage.getItem("data")) || [];
+let playlistCurrentSong;
+let playlistCurrentSongLikeBtn;
+let isPlaying = false;
+let isShuffling = false;
+let isRepeating = false;
+let currentSongIndex = Number(audioPlayer.src.split("").slice(-5, -4).join("")) || 0;
 // FUNCTIONS
 const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -56,26 +57,26 @@ const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
         return [];
     }
 });
-const postData = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const data = JSON.stringify(localStorage.getItem("data"));
-        const response = yield fetch("./data/songs.json", {
-            method: "POST",
-            body: data,
-        });
-        console.log(response);
-    }
-    catch (err) {
-        console.log(err);
-    }
-});
+// const postData = async (): Promise<void> => {
+//   try {
+//     const data = JSON.stringify(localStorage.getItem("data"));
+//     const response = await fetch("./data/songs.json", {
+//       method: "POST",
+//       body: data,
+//     });
+//     console.log(response);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 // functions - for searchbar
 const toggleSearchbar = () => {
-    if (searchbar) {
-        !searchbar.classList.contains("showSearchbar") &&
-            !searchbar.classList.contains("hideSearchbar")
-            ? searchbar.classList.add("showSearchbar")
-            : searchbar.classList.toggle("hideSearchbar");
+    if (!searchbar.classList.contains("showSearchbar") &&
+        !searchbar.classList.contains("hideSearchbar"))
+        searchbar.classList.add("showSearchbar");
+    else {
+        searchbar.value = "";
+        searchbar.classList.toggle("hideSearchbar");
     }
 };
 const searchSong = () => {
@@ -90,23 +91,18 @@ const searchSong = () => {
             filteredSongsContainer.style.display = "block";
     }
 };
-const handleKeydown = (e) => {
-    if (e.key === "Enter" && searchbar.value) {
-        searchSong();
-    }
-    else if (e.key === "Enter" && !searchbar.value) {
-        alert("Please enter title/ artist to search!");
-    }
-};
 // functions - render/update Display
 const renderPlaylist = () => __awaiter(void 0, void 0, void 0, function* () {
     playlist = yield fetchData();
     if (playlistContainer) {
         const playlistHTML = playlist
             .map((song) => {
-            return `
+            const src = `/music/music-${song.number}.mp3`;
+            playlistContainer.innerHTML += `
         <div class="playlistItem" data-audio="./music/music-${song.number}.mp3" data-title="${song.title}" data-artist="${song.artist}" onclick="togglePlay(${song.number})">
-          <p class="number" id="playlistItem-${song.number}">${song.number + 1}</p>
+          <p class="number" id="playlistItem-${song.number}">${currentSongIndex === song.number && !audioPlayer.paused
+                ? "<i class='fa fa-pause'></i>"
+                : song.number + 1}</p>
           <p class="title">${song.title}</p>
           <p class="artist">${song.artist}</p>
           <p class="length">${song.length}</p>
@@ -118,6 +114,7 @@ const renderPlaylist = () => __awaiter(void 0, void 0, void 0, function* () {
           viewBox="0 -960 960 960"
           >
             <path
+              fill=${song.isFavorite ? "red" : "white"}
               d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"
             />
           </svg>
@@ -125,71 +122,32 @@ const renderPlaylist = () => __awaiter(void 0, void 0, void 0, function* () {
         `;
         })
             .join("");
-        playlistContainer.innerHTML = playlistHTML;
-        currentTitle.textContent = playlist[0].title;
-        currentArtist.textContent = playlist[0].artist;
-        playlist.forEach((song) => updatePlaylistLikeBtn(song.number));
     }
 });
-const updatePlayBtnIcon = () => {
+const renderCurrentSongInfo = () => {
+    // update album image
+    coverImg.src = `./images/music-${currentSongIndex}.jpg`;
+    // update song title & artist
+    currentTitle.textContent = playlist[currentSongIndex].title;
+    currentArtist.textContent = playlist[currentSongIndex].artist;
+    // update play btn
     isPlaying
         ? (playBtn.innerHTML = `<path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>`)
         : (playBtn.innerHTML = `<path
-      id="playBtnPath"
-      d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
-    />`);
+    id="playBtnPath"
+    d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
+  />`);
+    // update like btn
+    localStoragePlaylist[currentSongIndex].isFavorite
+        ? (likeBtn.style.fill = "red")
+        : (likeBtn.style.fill = "white");
 };
-const updateCoverImg = (num) => {
-    coverImg.src = `./images/music-${num}.jpg`;
-};
-const updatePlaylistStatus = (num) => {
-    playlistCurrentSong = document.getElementById(`playlistItem-${num}`);
-    playlistContainer.childNodes.forEach((item) => {
-        const numContainer = document.getElementById(`playlistItem-${num}`);
-        numContainer.textContent = `${num + 1}`;
-    });
-    if (audioPlayer.src.includes(`/music/music-${num}.mp3`) &&
-        !audioPlayer.paused) {
-        playlistCurrentSong.innerHTML = "<i class='fa fa-pause'></i>";
-    }
-};
-const updatePlaylistLikeBtn = (num) => {
-    playlistCurrentSongLikeBtn = document.getElementById(`playlistItemLikeBtn-${num}`);
-    localStoragePlaylist = JSON.parse(localStorage.getItem("data"));
-    if (localStoragePlaylist && localStoragePlaylist[num].isFavorite) {
-        playlistCurrentSongLikeBtn.style.fill = "red";
-    }
-    else {
-        playlistCurrentSongLikeBtn.style.fill = "white";
-    }
-};
-const updateLikeBtn = (num = null) => {
-    const currentSongIndex = num ? num :
-        Number(audioPlayer.src.split("").slice(-5, -4).join(""));
-    localStoragePlaylist = JSON.parse(localStorage.getItem("data"));
-    if (localStoragePlaylist &&
-        localStoragePlaylist[currentSongIndex].isFavorite) {
-        likeBtn.style.fill = "red";
-    }
-    else {
-        likeBtn.style.fill = "white";
-    }
-};
-const toggleFavorite = (e, num = null) => {
-    localStoragePlaylist =
-        JSON.parse(localStorage.getItem("data")) || [];
-    let index;
-    num
-        ? (index = num)
-        : !audioPlayer.src
-            ? (index = 0)
-            : (index = Number(audioPlayer.src.split("").slice(-5, -4).join("")));
-    localStoragePlaylist[index].isFavorite === false
-        ? (localStoragePlaylist[index].isFavorite = true)
-        : (localStoragePlaylist[index].isFavorite = false);
+const toggleFavorite = () => {
+    localStoragePlaylist[currentSongIndex].isFavorite =
+        !localStoragePlaylist[currentSongIndex].isFavorite;
     localStorage.setItem("data", JSON.stringify(localStoragePlaylist));
-    playlist.forEach((i, index) => updatePlaylistLikeBtn(index));
-    updateLikeBtn(index);
+    renderPlaylist();
+    renderCurrentSongInfo();
 };
 const resetFilteredSongsContainer = () => {
     filteredSongsContainer.innerHTML = "";
@@ -198,16 +156,16 @@ const resetFilteredSongsContainer = () => {
 // functions - play/shuffle
 const togglePlay = (index) => {
     playBtn.innerHTML = `<path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>`;
-    searchbar.value = "";
-    if (searchbar === null || searchbar === void 0 ? void 0 : searchbar.classList.contains("showSearchbar"))
-        toggleSearchbar();
+    setTimeout(() => searchbar.classList.contains("showSearchbar") && !searchbar.classList.contains("hideSearchbar") ? toggleSearchbar() : null, 1500);
     const prevSrc = audioPlayer.src;
     const { number, title, artist } = playlist[index];
     audioPlayer.src = `./music/music-${number}.mp3`;
+    currentSongIndex =
+        Number(audioPlayer.src.split("").slice(-5, -4).join("")) || 0;
     if (isPlaying && prevSrc === audioPlayer.src) {
         audioPlayer.pause();
         isPlaying = false;
-        updatePlaylistStatus(index);
+        renderPlaylist();
         playBtn.innerHTML = `<path
             id="playBtnPath"
             d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"
@@ -218,15 +176,10 @@ const togglePlay = (index) => {
             console.error("Fehler beim Abspielen der Musik:", err);
         });
         isPlaying = true;
-        updatePlaylistStatus(index);
-        playBtn.innerHTML = `<path d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>`;
+        renderPlaylist();
+        renderCurrentSongInfo();
     }
-    currentTitle.textContent = title;
-    currentArtist.textContent = artist;
-    updatePlayBtnIcon();
-    updateCoverImg(index);
-    playlist.forEach((song, index) => updatePlaylistLikeBtn(index));
-    updateLikeBtn();
+    renderPlaylist();
 };
 const playPreviousSong = () => __awaiter(void 0, void 0, void 0, function* () {
     audioPlayer.pause();
@@ -238,31 +191,29 @@ const playPreviousSong = () => __awaiter(void 0, void 0, void 0, function* () {
 const playNextSong = () => __awaiter(void 0, void 0, void 0, function* () {
     audioPlayer.pause();
     isPlaying = false;
-    let index = Number(audioPlayer.src.split("").slice(-5, -4).join("")) + 1;
+    let index = currentSongIndex + 1;
     index >= playlist.length ? (index -= playlist.length) : null;
     togglePlay(index);
 });
-const play = (num) => {
-    if (!audioPlayer.src) {
-        togglePlay(0);
-    }
-    else {
-        let index = num ? num : Number(audioPlayer.src.split("").slice(-5, -4).join(""));
-        togglePlay(index);
-    }
+const play = () => {
+    currentSongIndex = !audioPlayer.src
+        ? 0
+        : Number(audioPlayer.src.split("").slice(-5, -4).join(""));
+    togglePlay(currentSongIndex);
 };
 const shuffle = (e) => __awaiter(void 0, void 0, void 0, function* () {
     if (!isShuffling) {
         e.target.style.fill = "slateblue";
         const randomIndex = Math.floor(Math.random() * (playlist === null || playlist === void 0 ? void 0 : playlist.length));
-        audioPlayer.src = `./music/music-${randomIndex}.mp3`;
+        currentSongIndex = randomIndex;
+        audioPlayer.src = `./music/music-${currentSongIndex}.mp3`;
         audioPlayer.play();
         audioPlayer.autoplay = true;
         isPlaying = true;
         isShuffling = true;
-        updatePlayBtnIcon();
-        currentTitle.textContent = playlist[randomIndex].title;
-        currentArtist.textContent = playlist[randomIndex].artist;
+        // updatePlayBtnIcon();
+        // currentTitle.textContent = playlist[randomIndex].title;
+        // currentArtist.textContent = playlist[randomIndex].artist;
     }
     else {
         e.target.style.fill = "white";
@@ -270,7 +221,7 @@ const shuffle = (e) => __awaiter(void 0, void 0, void 0, function* () {
         audioPlayer.autoplay = false;
         isPlaying = false;
         isShuffling = false;
-        updatePlayBtnIcon();
+        // updatePlayBtnIcon();
     }
 });
 const toggleRepeat = () => {
@@ -283,8 +234,7 @@ const toggleRepeat = () => {
         repeatOneBtn.style.fill = "white";
     }
     isRepeating = !isRepeating;
-    let index = Number(audioPlayer.src.split("").slice(-5, -4).join("")) + 1;
-    audioPlayer.paused && play(index);
+    audioPlayer.paused && play();
 };
 // functions - others
 const formatTime = (timeInSeconds) => {
@@ -306,10 +256,14 @@ const updateTime = () => {
 };
 const handleMenuBtnKlick = () => {
     container === null || container === void 0 ? void 0 : container.classList.toggle("active");
-    updateLikeBtn();
+    // updateLikeBtn();
 };
+const startMusicApp = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield renderPlaylist();
+    renderCurrentSongInfo();
+});
 // call the funktions
-document.addEventListener("DOMContentLoaded", () => renderPlaylist());
+document.addEventListener("DOMContentLoaded", startMusicApp);
 document.addEventListener("click", resetFilteredSongsContainer);
 menuBtn === null || menuBtn === void 0 ? void 0 : menuBtn.addEventListener("click", handleMenuBtnKlick);
 searchBtn === null || searchBtn === void 0 ? void 0 : searchBtn.addEventListener("click", toggleSearchbar);
